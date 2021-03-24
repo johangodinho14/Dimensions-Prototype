eda62684534b13c56192f3854d42c4a4eb8e4355
jesusgodinho59a <jesus.godinho@59a.co.uk>
2021-03-24 16:35:24 +0000

    functioning version of website

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ recognition.onresult = function(event) {
     final_transcript = capitalize(final_transcript);
     Username.value = linebreak(final_transcript);
 };
-
 recognition.onerror = function(event) {
     if (event.error == 'no-speech') {
         console.log("no speech");
@@ -62,13 +61,20 @@ function capitalize(s) {
   return s.replace(first_char, function(m) { return m.toUpperCase(); });
 }
 
-//color
-//fonts
-//alerts
-
-
 function categories(){
     window.location.href = 'http://localhost:5500/categories.html';
+}
+
+function searchTask(){
+    window.location.href = 'http://localhost:5500/bed-making.html';
+}
+
+function selectColors(){
+    window.location.href = 'http://localhost:5500/colour_pref.html';    
+}
+
+function selectFont(){
+    window.location.href = 'http://localhost:5500/font_pref.html';
 }
 
 function update_c_preferences(option){
@@ -82,31 +88,64 @@ function update_c_preferences(option){
     window.location.reload();
 }
 
+function update_f_preferences(option){
+    let new_preset = {
+        font:option,
+        colorScheme:JSON.parse(localStorage.getItem(localStorage.getItem('currentUser').toUpperCase()))['colorScheme'],
+        alerts:true
+    }
+    localStorage.setItem(localStorage.getItem('currentUser').toUpperCase(),JSON.stringify(new_preset));
+    console.log(JSON.parse(localStorage.getItem(localStorage.getItem('currentUser').toUpperCase())));
+    window.location.reload();
+}
+
 function alterPage(){
     let background;
     let foreground;
+    if(localStorage.getItem('currentUser') == null){return};
     preferences = JSON.parse(localStorage.getItem(localStorage.getItem('currentUser').toUpperCase()));
     console.log(preferences)
+    if(preferences == null){return};
     if (preferences['colorScheme'] === 'cool-colours'){
         background = '#A4CAEE';
         foreground = 'black';
     }
+    if(preferences['colorScheme'] === 'warm-colours'){
+        background = '#EB7B15';
+        foreground = '#F11E10';
+    }
+    if(preferences['colorScheme'] === 'purple-colours'){
+        background = '#8A5991';
+        foreground = '#8E2991';
+    }
 
+    //Change the color scheme of the page based on user preferences
     document.body.style.backgroundColor = background;
+    document.getElementsByClassName('container')[0].style.color = foreground;
+    try{
+        document.getElementsByTagName('h1')[0].style.color = foreground;
+        let buttons_list = document.getElementsByTagName('button');
+        for(let i=0;i<buttons_list.length;i++){
+            buttons_list[i].style.backgroundColor = background;
+            buttons_list[i].style.color = foreground;
+            buttons_list[i].style.fontSize  = preferences['font'];
+        }
+    }
+    catch{
+        console.log('some elements were not found');
+    }
+    finally{
+        //Change the font-size of the page based on user preferences
+        document.getElementsByTagName('div')[0].style.fontSize = preferences['font'];
+    }
 }
 
-function selectColors(){
-    window.location.href = 'http://localhost:5500/colour_pref.html';    
-    alterPage();
- }
-
-
 function login(){
-    username = document.getElementById('Username').value;
+    username    = document.getElementById('Username').value;
     localStorage.setItem('currentUser',username);
-    const info     = document.getElementById('info');
+    const info  = document.getElementById('info');
     preferences = JSON.parse(localStorage.getItem(username.toUpperCase()));
-    console.log(preferences)
+    console.log(preferences);
     //Quick authentication 
     if(username.toUpperCase() === 'BOB' || username.toUpperCase() === 'HARRY'){
         if(preferences !== null){
